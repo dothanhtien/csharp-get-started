9943e79031e6fcd3e37169d2e9406c2919c98a4b
Tien Do Thanh <dothanhtien95@gmail.com>
2024-02-29 07:51:11 +0700

    generate seed data

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
+using CSharpGetStarted.Data;
 using CSharpGetStarted.Extensions;
 using CSharpGetStarted.Middlewares;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,5 +23,19 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
 
 app.Run();
