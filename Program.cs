01ca58f6cabe726a689baf92cfb73285066ac174
Tien Do Thanh <dothanhtien95@gmail.com>
2024-02-29 07:16:44 +0700

    add exception middleware

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using CSharpGetStarted.Extensions;
+using CSharpGetStarted.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,8 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseHttpsRedirection();
 
