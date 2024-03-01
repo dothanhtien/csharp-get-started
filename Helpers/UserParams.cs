58dc805f9ba189edb75d787d0933b7a580646453
Tien Do Thanh <dothanhtien95@gmail.com>
2024-03-01 11:13:02 +0700

    add sorting

--- Helpers/UserParams.cs
+++ Helpers/UserParams.cs
@@ -15,5 +15,6 @@ namespace CSharpGetStarted.Helpers
         public string Gender { get; set; }
         public int MinAge { get; set; } = 18;
         public int MaxAge { get; set; } = 100;
+        public string OrderBy { get; set; } = "lastActive";
     }
 }
