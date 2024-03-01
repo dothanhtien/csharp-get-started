120d408e80022490a136f0a0e25691e9dca18f58
Tien Do Thanh <dothanhtien95@gmail.com>
2024-03-01 22:52:51 +0700

    add message entity

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -11,6 +11,7 @@ namespace CSharpGetStarted.Data
 
         public DbSet<AppUser> Users { get; set; }
         public DbSet<UserLike> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -30,6 +31,16 @@ namespace CSharpGetStarted.Data
                 .WithMany(l => l.LikedByUsers)
                 .HasForeignKey(s => s.TargetUserId)
                 .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Recipient)
+                .WithMany(u => u.MessagesReceived)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(u => u.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
