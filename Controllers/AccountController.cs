62e108316a8579d2fb7aecac676d9f0a316697fa
Tien Do Thanh <dothanhtien95@gmail.com>
2024-03-02 13:20:20 +0700

    update account controller

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,8 +1,8 @@
 using AutoMapper;
-using CSharpGetStarted.Data;
 using CSharpGetStarted.DTOs;
 using CSharpGetStarted.Entities;
 using CSharpGetStarted.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,13 +10,13 @@ namespace CSharpGetStarted.Controllers
 {
     public class AccountController : BaseApiController
     {
-        private readonly DataContext _context;
+        private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
         private readonly IMapper _mapper;
 
-        public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, IMapper mapper)
         {
-            _context = context;
+            _userManager = userManager;
             _tokenService = tokenService;
             _mapper = mapper;
         }
@@ -28,40 +28,49 @@ namespace CSharpGetStarted.Controllers
 
             var user = _mapper.Map<AppUser>(registerDto);
 
-            _context.Users.Add(user);
+            user.UserName = registerDto.Username.Trim().ToLower();
 
-            await _context.SaveChangesAsync();
+            var result = await _userManager.CreateAsync(user, registerDto.Password.Trim());
 
-            var result = new UserDto
-            {
-                Id = user.Id,
-                UserName = user.UserName,
-                Token = _tokenService.CreateToken(user)
-            };
+            if (!result.Succeeded) return BadRequest(result.Errors);
 
-            return Ok(result);
+            return Ok
+            (
+                new UserDto
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Token = _tokenService.CreateToken(user)
+                }
+            );
         }
 
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName.Equals(loginDto.Username.Trim().ToLower()));
+            var user = await _userManager
+                .Users
+                .Include(p => p.Photos)
+                .SingleOrDefaultAsync(x => x.UserName.Equals(loginDto.Username.Trim().ToLower()));
 
             if (user == null) return Unauthorized("Invalid username or password");
 
-            var result = new UserDto
-            {
-                Id = user.Id,
-                UserName = user.UserName,
-                Token = _tokenService.CreateToken(user)
-            };
+            var result = await _userManager.CheckPasswordAsync(user, loginDto.Password.Trim());
 
-            return Ok(result);
+            if (!result) return Unauthorized("Invalid username or password");
+
+            return Ok(new UserDto
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Token = _tokenService.CreateToken(user)
+                }
+            );
         }
 
         private async Task<bool> UserExist(string username)
         {
-            return await _context
+            return await _userManager
                 .Users
                 .AnyAsync(x => 
                     x.UserName.Equals(username.Trim().ToLower())
