f09b10972a23efdad3289a2145ac4947a5ccb07c
Tien Do Thanh <dothanhtien95@gmail.com>
2024-03-02 12:44:40 +0700

    update seed

--- Data/Seed.cs
+++ Data/Seed.cs
@@ -1,16 +1,15 @@
-using System.Security.Cryptography;
-using System.Text;
 using System.Text.Json;
 using CSharpGetStarted.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace CSharpGetStarted.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers(DataContext context)
+        public static async Task SeedUsers(UserManager<AppUser> userManager)
         {
-            if (await context.Users.AnyAsync()) return;
+            if (await userManager.Users.AnyAsync()) return;
 
             var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
 
@@ -22,10 +21,8 @@ namespace CSharpGetStarted.Data
             {
                 user.UserName = user.UserName.Trim().ToLower();
 
-                context.Users.Add(user);
+                await userManager.CreateAsync(user, "Pa$$w0rd");
             }
-
-            await context.SaveChangesAsync();
         }
     }
 }
