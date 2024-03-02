81228deea1ae9020ca0af2c42f8607bef73f23f3
Tien Do Thanh <dothanhtien95@gmail.com>
2024-03-02 12:40:02 +0700

    add migration

--- Extensions/IdentityServiceExtensions.cs
+++ Extensions/IdentityServiceExtensions.cs
@@ -1,5 +1,8 @@
 using System.Text;
+using CSharpGetStarted.Data;
+using CSharpGetStarted.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 
 namespace CSharpGetStarted.Extensions
@@ -8,6 +11,14 @@ namespace CSharpGetStarted.Extensions
     {
         public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
         {
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+                .AddRoles<AppRole>()
+                .AddRoleManager<RoleManager<AppRole>>()
+                .AddEntityFrameworkStores<DataContext>();
+
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(opt =>
                 {
