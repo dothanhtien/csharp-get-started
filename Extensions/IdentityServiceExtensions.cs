384e680340cdd445659b097296702b618967f40f
Tien Do Thanh <dothanhtien95@gmail.com>
2024-03-02 21:42:57 +0700

    add policy based authorisation

--- Extensions/IdentityServiceExtensions.cs
+++ Extensions/IdentityServiceExtensions.cs
@@ -31,6 +31,12 @@ namespace CSharpGetStarted.Extensions
                     };
                 });
 
+            services.AddAuthorization(opt => 
+            {
+                opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+            });
+
             return services;
         }
     }
