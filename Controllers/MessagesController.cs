399ea98a71e2bb673e62a3c963e910b4ae9597a1
Tien Do Thanh <dothanhtien95@gmail.com>
2024-03-02 12:11:05 +0700

    update messages controller

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using CSharpGetStarted.DTOs;
 using CSharpGetStarted.Entities;
 using CSharpGetStarted.Extensions;
+using CSharpGetStarted.Helpers;
 using CSharpGetStarted.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -50,6 +51,62 @@ namespace CSharpGetStarted.Controllers
             if (await _messageRepository.SaveAllAsync()) return Ok(_mapper.Map<MessageDto>(message));
 
             return BadRequest("An error occurred when sending message");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<PagedList<MessageDto>>> GetMessagesForUser([FromQuery]MessageParams messageParams)
+        {
+            messageParams.Username = User.GetUsername();
+
+            var messages = await _messageRepository.GetMessagesForUser(messageParams);
+
+            Response.AddPaginationHeader
+                (
+                    new PaginationHeader
+                    (
+                        messages.CurrentPage, 
+                        messages.PageSize, 
+                        messages.TotalCount, 
+                        messages.TotalPages
+                    )
+                );
+
+            return Ok(messages);
+        }
+
+        [HttpGet("thread/{username}")]
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessageThread(string username)
+        {
+            var currentUsername = User.GetUsername();
+
+            return Ok(await _messageRepository.GetMessageThread(currentUsername, username));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteMessage(int id)
+        {
+            var username = User.GetUsername();
+
+            var message = await _messageRepository.GetMessage(id);
+
+            if (message == null) return NotFound();
+
+            if (message.SenderUsername != username && message.RecipientUsername != username)
+            {
+                return Unauthorized();
+            }
+
+            if (message.SenderUsername == username) message.SenderDeleted = true;
+            if (message.RecipientUsername == username) message.RecipientDeleted = true;
+
+            if (message.SenderDeleted && message.RecipientDeleted)
+            {
+                _messageRepository.DeleteMessage(message);
+            }
+
+            if (await _messageRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("An error occurred when deleting message");
         }
     }
 }
